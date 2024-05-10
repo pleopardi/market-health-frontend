25b6d965341008e419fa818d50a534e493ec87e5
Paolo Leopardi <leopardipaolo@gmail.com>
2024-05-10 16:19:01 +0200

    feat(home): update title

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import type {DocumentHead} from '@builder.io/qwik-city';
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
+      <h1>Hello world 👋</h1>
       <p>
         Can't wait to see what you build with qwik!
         <br />
@@ -15,11 +15,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Market Health Chart',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'Market Health Chart',
     },
   ],
 };
