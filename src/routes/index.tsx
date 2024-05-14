ef6a42c28432a23a9a4baddf8032e467d1500107
Paolo Leopardi <leopardipaolo@gmail.com>
2024-05-14 15:47:26 +0200

    feat(home): add chart

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,34 @@
-import {component$} from '@builder.io/qwik';
+import {component$, useSignal, useVisibleTask$} from '@builder.io/qwik';
 import type {DocumentHead} from '@builder.io/qwik-city';
+import {createChart} from 'lightweight-charts';
+import {getDataPoints} from 'modules/chart';
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hello world 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
-  );
+  const chartRef = useSignal<HTMLElement>();
+
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(() => {
+    if (!chartRef.value) {
+      return;
+    }
+
+    const chart = createChart(
+      chartRef.value,
+      chartRef.value.getBoundingClientRect(),
+    );
+
+    const candlestickSeries = chart.addCandlestickSeries({
+      borderVisible: false,
+      downColor: '#EF5350',
+      upColor: '#26A69A',
+      wickDownColor: '#EF5350',
+      wickUpColor: '#26A69A',
+    });
+
+    getDataPoints().then(candlestickSeries.setData.bind(candlestickSeries));
+  });
+
+  return <div ref={chartRef} class="h-full w-full" />;
 });
 
 export const head: DocumentHead = {
