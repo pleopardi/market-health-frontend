f77ec45f593740183dbf288b6a151c97138070b1
Paolo Leopardi <leopardipaolo@gmail.com>
2024-06-04 12:02:23 +0200

    refactor(getBars): HTTP request

--- src/modules/chart/repository/getBars.ts
+++ src/modules/chart/repository/getBars.ts
@@ -1,6 +1,32 @@
-import {daily} from '../fixtures';
+import {getBarsResponseDto} from '../dtos';
 import {type GetBars} from '../ports';
+import {notFoundError, requestError, validationError} from 'utilities/error';
+import {failure, success} from 'utilities/result';
 
-const getBars: GetBars = () => Promise.resolve(daily);
+const getBars: GetBars = async ({ticker, timeframe}) => {
+  const result = await fetch(
+    `http://localhost:8000/bars/${ticker}?timeframe=${timeframe}`,
+  );
+
+  if (result.status === 200) {
+    const data = await result.json();
+
+    try {
+      getBarsResponseDto.parse(data);
+    } catch (error) {
+      return failure(validationError(error, 'getBars'));
+    }
+
+    return success(data);
+  }
+
+  if (result.status === 404) {
+    return failure(notFoundError(`Ticker ${ticker} not found`, 'getBars'));
+  }
+
+  return failure(
+    requestError(`Request failed with status ${result.status}`, 'getBars'),
+  );
+};
 
 export default getBars;
